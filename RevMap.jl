8d4754847966666a6d99229cd906665b7e8cd3d5
matdander <matdander@gmail.com>
2015-09-09 16:29:51 -0400

    Just cleaned up a few things.

--- RevMap.jl
+++ RevMap.jl
@@ -95,7 +95,7 @@ function TLDR(mu,L, num_lines, EL, spectra_dates, num_spectra_samples,continuum_
 	  #= CONVERGANCE PARAMETERS =#
 	G = 10.0 # only for first iteration 1.5 otherwise
 	tau = 1.1
-	sigma = 0.75
+	sigma = 0.9
 	#ADMM ARRAYS:
 	Con_Arr = zeros(num_lines)
 	X =   zeros((size(L,1),num_tdf_times))
@@ -150,14 +150,12 @@ function TLDR(mu,L, num_lines, EL, spectra_dates, num_spectra_samples,continuum_
 	      #B = HT*slice*vec(L[l,:])+rho[it,l]*GammaT*Gamma*vec(Z_s[it,l,:])
 	      B = HT*slice*vec(L[l,:])
 		    Z[l,:] = A * B
-	
 	    #Step 2 done!
 	    #Step 3:
 	      #eq =U[l,:]+(rho[l]/2.0)*(X[l,:]-Z[l,:])
 	      U[l,:] = U[l,:]+(rho[l]/2.0)*(X[l,:]-Z[l,:])
 	      #= OR =#   
 				#U[l,:] = HT * slice * ( H * vec(Z[l,:]) - vec(L[l,:]))		#UPDATE MULTIPLIERS
-	      
 	    #Step 3 done!
 	    # Evaluation:
 	      eps_abs = 0.0    #Must be >= 0
@@ -172,25 +170,6 @@ function TLDR(mu,L, num_lines, EL, spectra_dates, num_spectra_samples,continuum_
 			  #eta = ell2norm(R)*tau_dual_previous[l] / (ell2norm(S)*tau_prim_previous[l])	#OPTION 2
 				phi_previous[l] = phi[l]
 				phi[l] = max(ell2norm(R/tau_prim[l]),ell2norm(S)/tau_dual[l])
-				if l == 1
-				e1 = (H*vec(X[l,:])-vec(L[l,:]))
-				ext =  e1'*squeeze(W[l,:,:],1)*e1
-				Mod = Model(X[l,:],H) 
-				#println("Line = ", l, " Chi2 = ",Chi2(Mod,L[l,:],EL[l,:])/num_tdf_times, " Mat = ",ext/num_tdf_times)
-				#println("Line = ", l, " Chi2 = ",Chi2(Model(Z[l,:],H),L[l,:],EL[l,:])/num_tdf_times)
-				#println("Phi = ", phi[l])
-				#println("||S||2: ", ell2norm(S), "    ||R||2: ", ell2norm(R))
-				#println("Tau_Dual: ", tau_dual[l], " Tau_prim: ", tau_prim[l])
-				#println("ratio dual: ", ell2norm(S)/tau_dual[l]," ratio prime ", ell2norm(R)/tau_prim[l])
-				#println(" Eta: ",eta, "    Phi: ", phi[l])
-				#tmp = ext + rho[l]/2.0*ell2norm(Z[l,:]-Z_s[l,:])^2
-				#println("Total FCN: ", tmp)
-				#println("Rho: ",rho[l]," U: ",mean(U[l,:]))
-				#Mod_act = Model(X_act,H)
-				#e2 = H*vec(X_act)-vec(L[l,:])
-				#ext2 =  e2'*squeeze(W[l,:,:],1)*e2
-				#println("--------------------------------------")
-	     	end
 				if phi[l] <= 1.0
 					Con_Arr[l] = 1
 				end
@@ -222,12 +201,15 @@ function TLDR(mu,L, num_lines, EL, spectra_dates, num_spectra_samples,continuum_
 	end
 	Mod = Model(X[1,:],H) 
 	outarr = [it-1, Chi2(Mod,L[1,:],EL[1,:])/num_tdf_times]
+	if converged != 1
+		println("Mu = ", mu," did not converge")
+	end
 	outarr
 end
 
 
 num_mus=500
-mu = linspace(45,115000.0,num_mus)
+mu = linspace(40,120000.0,num_mus)   #A GOOD RANGE FOR SIGMA = 0.75 On SYNTH DATA : TDF == 0.04
 
 chi2 = zeros((num_mus))
 its = zeros((num_mus))
