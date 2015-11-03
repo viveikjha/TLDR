808b1878970233f8da14a8835ac2c70ac731e23f
matdander <matdander@gmail.com>
2015-11-03 17:04:28 -0500

    Implemented some data structures, more to do. Also added some functions in the update steps to clean up the ADMM loop.

--- tst.jl
+++ tst.jl
@@ -1,22 +1,12 @@
-#Building Difference Matrices
-num_spectra_samples = 10
-num_tdf_times = 10
-D = zeros(num_spectra_samples,num_tdf_times)
-
-for i in 1:num_spectra_samples
-	for j in 1:num_tdf_times
-		if (i+1) == j
-			D[i,j] = 1
-		end
-		if (i-1) == j
-			D[i,j] = -1
-		end
-	end
-end
-println(D)
-
-function ell2_prox_op(X,mu)
-	X_returned = 1.0/(1.0*2.0*mu)*X
+type im
+	x::Array{Float64}
+	y::Array{Float64}
 end
 
+im1 = im
+im2 = im
 
+#im1.x = [1,2,3,4]
+#im1.y = [2,5,4,6]
+
+println(im1)
