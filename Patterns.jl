984ae0ca6df5e92b0767f52e7dd06f7e0a839e40
Matt Anderson <matdander@gmail.com>
2018-02-09 22:11:04 -0800

    Added some more patterns to Patterns.jl.

--- Patterns.jl
+++ Patterns.jl
@@ -4,18 +4,29 @@ nx=50
 ny=20
 
 
-checkerboard=zeros(nx,ny)
+checkerboard=zeros(nx,ny); n=2; #n sets the size of the checks.
 circle=zeros(nx,ny); center=[nx/2,ny/2]; radius=(min(nx,ny)/4);
+ring=zeros(nx,ny); center=[nx/2,ny/2]; radius=(min(nx,ny)/4); width=3
 for i in collect(1:nx)
   for j in collect(1:ny)
-    if i%2==0 && j %2 ==0 && i != nx && j!=ny
-      checkerboard[i,j]=1.0
+    if i%(2*n)==0 && j%(2*n) ==0 && i!=nx && j!=ny
+      for p in collect(0:n-1)
+        for q in collect(0:n-1)
+          checkerboard[i-p,j-q]=1.0
+        end
+      end
     end
     if sqrt((i-center[1])^2+(j-center[2])^2)<=radius
       circle[i,j]=1.0
     end
+
+    if sqrt((i-center[1])^2+(j-center[2])^2)<=(radius+width) && sqrt((i-center[1])^2+(j-center[2])^2)>=(radius-width)
+      ring[i,j]=1.0
+    end
+
   end
 end
 figure()
-imshow(circle,interpolation="None",cmap="Greys",origin="Lower")
+imshow(ring,interpolation="None",cmap="Greys",origin="Lower")
+colorbar()
 show()
